2281dbf61871862916a9dada8d27d2d4abeba9eb
Release Engineering <cwalden@emmoco.com>
2013-10-08 14:11:07 -0500

    mkexamples.sh: auto-commit

--- EK-TM4C123GXL/Platform-EK-TM4C123GXL/rules.mk
+++ EK-TM4C123GXL/Platform-EK-TM4C123GXL/rules.mk
@@ -6,12 +6,12 @@ OUTFILE = $(OUTDIR)/$(MAIN).out
 BINFILE = $(OUTDIR)/$(MAIN).bin
 OBJECTS = $(OUTDIR)/$(MAIN).obj $(OUTDIR)/$(APPNAME).obj $(OUTDIR)/Hal.obj 
 
-TOOLS ?= c:/progs/gcc-arm-none-eabi/bin/arm-none-eabi
+TOOLS ?= $(EMMOCO-ROOT)/armtools/bin/arm-none-eabi
 CC = $(TOOLS)-gcc
 LD = $(TOOLS)-ld
 OBJCOPY = $(TOOLS)-objcopy
 SIZE = $(TOOLS)-size
-LMFLASH = c:/progs/lmflash/lmflash
+LMFLASH = $(EMMOCO-ROOT)/armtools/LMFlash/LMFlash
 COPTS = -mthumb -mcpu=cortex-m4 -std=gnu99 -O2 -w -ffunction-sections -fdata-sections -fpack-struct=1 -fno-strict-aliasing -fomit-frame-pointer -c -g
 CFLAGS = -Dsourcerygxx -DTARGET_IS_BLIZZARD_RA1 -DPART_LM4F120H5QR -I$(PLATFORM)/Hal -I$(PLATFORM)/StellarisWare -IEm $(COPTS)
 LDOPTS = -Map=$(OUTDIR)/$(MAIN).map -L$(PLATFORM)/StellarisWare/driverlib/gcc-cm4f -ldriver-cm4f -T $(PLATFORM)/ek-lm4f232.ld --entry ResetISR --gc-sections
