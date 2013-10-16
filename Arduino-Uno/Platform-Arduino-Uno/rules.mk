fd2f22f29b8abbf7801e39470e7ab51dcdc41e4e
Release Engineering <cwalden@emmoco.com>
2013-10-15 22:37:54 -0500

    mkexamples.sh: auto-commit

--- Arduino-Uno/Platform-Arduino-Uno/rules.mk
+++ Arduino-Uno/Platform-Arduino-Uno/rules.mk
@@ -4,11 +4,10 @@ WINAVR = c:/progs/WinAVR-20100110
 TOOLS ?= $(WINAVR)/bin/avr
 MCU = atmega328p
 
-COPTS = -c -mmcu=$(MCU)
-CFLAGS = -I$(PLATFORM)/Hal -IEm -I$(WINAVR)/avr/include $(COPTS)
+COPTS = -mmcu=$(MCU) -I$(WINAVR)/avr/include
 LFLAGS = -mmcu=$(MCU) --entry __init -u __init -nostdlib -Wl,-Map=$(OUTDIR)/$(MAIN).map,--gc-sections
 
-EXEC = c:/emmoco/workspace/Platform-Arduino-Uno/Avr/avrdude -C $(PLATFORM)/Avr/avrdude.conf -c stk500v1 -p $(MCU) -P COM7
+EXEC = $(PLATFORM)/Avr/avrdude -C $(PLATFORM)/Avr/avrdude.conf -c stk500v1 -p $(MCU) -P COM7
 
 $(OUTFILE): $(OBJECTS)
 	$(CC) $(LFLAGS) -o $@ $(PLATFORM)/Avr/crt0.o $^ -T $(PLATFORM)/Avr/lnk.cmd
