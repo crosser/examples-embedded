322a13ed344b0c048db691cc23e590b4e49c0a2b
Release Engineering <cwalden@emmoco.com>
2013-11-06 15:44:44 -0600

    mkexamples.sh: auto-commit

--- Arduino-Uno/Platform-Arduino-Uno/rules.mk
+++ Arduino-Uno/Platform-Arduino-Uno/rules.mk
@@ -1,14 +1,14 @@
 include $(PLATFORM)/common.mk
+include $(PLATFORM)/local.mk
 
-WINAVR = c:/progs/WinAVR-20100110
-TOOLS ?= $(WINAVR)/bin
+TOOLS ?= $(WINAVR-PATH)/bin
 GCCARCH = avr
 MCU = atmega328p
 
-COPTS = -mmcu=$(MCU) -I$(WINAVR)/avr/include
+COPTS = -mmcu=$(MCU) -I$(WINAVR-PATH)/avr/include
 LFLAGS = -mmcu=$(MCU) --entry __init -u __init -nostdlib -Wl,-Map=$(OUTDIR)/$(MAIN).map,--gc-sections
 
-EXEC = $(PLATFORM)/Avr/avrdude -C $(PLATFORM)/Avr/avrdude.conf -c stk500v1 -p $(MCU) -P COM7 -q -q -V -F -U flash:w:$(HEXFILE)
+EXEC = $(PLATFORM)/Avr/avrdude -C $(PLATFORM)/Avr/avrdude.conf -c stk500v1 -p $(MCU) -P $(COM-PORT) -q -q -V -F -U flash:w:$(HEXFILE)
 
 $(OUTFILE): $(OBJECTS)
 	$(CC) $(LFLAGS) -o $@ $(PLATFORM)/Avr/crt0.o $^ -T $(PLATFORM)/Avr/lnk.cmd
