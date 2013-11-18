3bbca3de13d9768e70b3ddeb2d33509dbe6b0b2b
Release Engineering <cwalden@emmoco.com>
2013-11-17 19:48:34 -0600

    mkexamples.sh: auto-commit

--- MSP-EXP430G2/Platform-MSP-EXP430G2/rules.mk
+++ MSP-EXP430G2/Platform-MSP-EXP430G2/rules.mk
@@ -7,7 +7,11 @@ MCU = msp430g2553
 COPTS = -mmcu=$(MCU)
 LDOPTS = -mmcu=$(MCU) -Wl,-Map=$(OUTDIR)/$(MAIN).map,--gc-sections
 
+ifeq (,$(findstring Windows,$(OS)))
+EXEC = $(EMMOCO-ROOT)/msptools/bin/mspdebug rf2500 "prog $(OUTFILE)" 2>&1
+else
 EXEC = $(EMMOCO-ROOT)/msptools/bin/MSP430Flasher -i USB -m AUTO -e ERASE_MAIN -n $(MCU) -w $(HEXFILE) -v -z [VCC] -g -s -q 
+endif
 
 $(OUTFILE): $(OBJECTS)
 	$(CC) -o $(OUTFILE) $^ $(LDOPTS)
