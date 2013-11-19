c327e86a83a68d7365621dccbf60ce54f7b8536c
Release Engineering <cwalden@emmoco.com>
2013-11-19 17:45:34 -0600

    mkexamples.sh: auto-commit

--- EFM32-STK/Platform-EFM32-STK/local.mk
+++
@@ -1,8 +0,0 @@
-# absolute path to your EFM32 install directory
-EFM-PATH = C:/Users/Bob/AppData/Roaming/energymicro
-
-# absolute path to this project
-PLATFORM-PATH = C:/emmoco/gecko/workspace/Platform-EFM32-STK
-
-# EFM32 family (efm32g, efm32gg)
-FAMILY = efm32gg
