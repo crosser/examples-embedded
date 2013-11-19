cc1ca9b96660a9c30fdae7df1d7191f3c1eb3ceb
Release Engineering <cwalden@emmoco.com>
2013-11-19 14:22:47 -0600

    mkexamples.sh: auto-commit

--- EFM_Gxxx_STK/Platform-EFM_Gxxx_STK/local.mk
+++
@@ -1,5 +0,0 @@
-# absolute path to your EMF install directory
-EFM-PATH = C:/Users/Bob/AppData/Roaming/energymicro
-
-#absolute path to this project
-PLATFORM-PATH = C:/emmoco/gecko/workspace/Platform-EMF_Gxxx_STK
