dc2ff4574bf06e2681d69a679b7d24bf313c7e5d
Release Engineering <cwalden@emmoco.com>
2014-02-15 18:53:44 -0600

    mkexamples.sh: auto-commit

--- MSP-EXP430x/Ex_Beacon-MSP-EXP430x/Ex_Beacon-Prog.c
+++ MSP-EXP430x/Ex_Beacon-MSP-EXP430x/Ex_Beacon-Prog.c
@@ -2,18 +2,19 @@
 #include "Hal.h"
 
 const uint8_t UUID[] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xAA,0xBB,0xCC,0xDD,0xEE,0xFF};
-const uint16_t MAJOR = 20;
-const uint16_t MINOR = 500;
 
 static void buttonHandler(void);
 static void commandDoneHandler(void);
+
 static bool beaconEnabled = false;
+static Ex_Beacon_info_t infoVal = {20, 500};
+static bool restartFlag = false;
 
 void main() {
 	Hal_init();
     Hal_buttonEnable(buttonHandler);
     Ex_Beacon_onCommandDone(commandDoneHandler);
-    Ex_Beacon_setBeaconInfo(UUID, MAJOR, MINOR);
+    Ex_Beacon_setBeaconInfo(UUID, infoVal.majorId, infoVal.minorId);
 	Ex_Beacon_start();    
 	Hal_idleLoop();
 }
@@ -24,7 +25,7 @@ static void buttonHandler(void) {
     Hal_ledOff();
     Hal_debugPulse(1);
     if (!beaconEnabled) {
-        Ex_Beacon_beacon();
+        Ex_Beacon_beacon(0);
     }
     else {
         Ex_Beacon_accept(true);
@@ -34,10 +35,10 @@ static void buttonHandler(void) {
 
 static void commandDoneHandler(void) {
     if (beaconEnabled) {
-        Hal_connected();
+        Hal_ledOn();
     }
     else {
-        Hal_disconnected();
+        Hal_ledOff();
     }
 }
 
@@ -47,4 +48,22 @@ void Ex_Beacon_connectHandler(void) {
 
 void Ex_Beacon_disconnectHandler(void) {
     Hal_disconnected();
+    if (restartFlag) {
+        restartFlag = false;        
+        Ex_Beacon_setBeaconInfo(UUID, infoVal.majorId, infoVal.minorId);
+        Ex_Beacon_reset();
+        Ex_Beacon_start();
+    }
+    else if (beaconEnabled) {
+        Ex_Beacon_beacon(0);
+    }
+}
+
+void Ex_Beacon_info_fetch(Ex_Beacon_info_t* output) {
+    *output = infoVal;
+}
+
+void Ex_Beacon_info_store(Ex_Beacon_info_t* input) {
+    infoVal = *input;
+    restartFlag = true;
 }
