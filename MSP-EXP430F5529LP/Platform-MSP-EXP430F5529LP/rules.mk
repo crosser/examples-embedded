fba314afe8b700c2bdef4b7ca1576215fd7179d6
Release Engineering <cwalden@emmoco.com>
2014-02-27 16:05:09 -0600

    mkexamples.sh: auto-commit

--- MSP-EXP430F5529LP/Platform-MSP-EXP430F5529LP/rules.mk
+++ MSP-EXP430F5529LP/Platform-MSP-EXP430F5529LP/rules.mk
@@ -2,6 +2,10 @@ include $(PLATFORM)/common.mk
 -include $(PLATFORM)/local.mk
 -include local.mk
 
+ifeq (,$(COMPORT))
+	COMPORT = USB
+endif
+
 TOOLS ?= $(EMMOCO-ROOT)/msptools/bin
 GCCARCH = msp430
 MCU = msp430f5529
@@ -9,7 +13,7 @@ MCU = msp430f5529
 COPTS = -mmcu=$(MCU)
 LDOPTS = -mmcu=$(MCU) -Wl,-Map=$(OUTDIR)/$(MAIN).map,--gc-sections
 
-EXEC = $(EMMOCO-ROOT)/msptools/bin/MSP430Flasher -i USB -m AUTO -e ERASE_MAIN -n $(MCU) -w $(HEXFILE) -v -z [VCC] -g -s -q 
+EXEC = $(EMMOCO-ROOT)/msptools/bin/MSP430Flasher -i $(COMPORT) -m AUTO -e ERASE_MAIN -n $(MCU) -w $(HEXFILE) -v -z [VCC] -g -s
 
 $(OUTFILE): $(OBJECTS)
 	$(CC) -o $(OUTFILE) $^ $(LDOPTS)
